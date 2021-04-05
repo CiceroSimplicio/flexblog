65fb40ad157621e43c83fd9b65c8b726268fc893
CiceroSimplicio <ciceroandrade95@gmail.com>
2021-04-04 22:18:06 -0300

    adicao do menu mobile

---
+++ script.js
@@ -0,0 +1,20 @@
+let show = true;
+
+const menuSection = document.getElementById('menu');
+const menuToggle = document.getElementById('menu-toggle');
+const menuNav = document.getElementById('menu-nav');
+
+menuToggle.addEventListener('click', ()=> {
+
+    document.body.style.overflow = show ? 'hidden' : 'initial';
+
+    menuSection.classList.toggle('on', show);
+    show = !show;
+})
+
+menuNav.addEventListener('click', ()=> {
+    menuSection.classList.remove('on');
+    
+    document.body.style.overflow = show ? 'hidden' : 'initial';
+    show = !show;
+})
