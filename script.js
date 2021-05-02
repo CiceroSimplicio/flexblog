21041c4f5efaf20c77feff947cb7a3cf82c84964
CiceroSimplicio <ciceroandrade95@gmail.com>
2021-05-02 14:21:55 -0300

    scroll suave adicionado

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// menu toggle
 let show = true;
 
 const menuSection = document.getElementById('menu');
@@ -8,10 +10,38 @@ menuToggle.addEventListener('click', ()=> {
     document.body.style.overflow = show ? 'hidden' : 'initial';
     menuSection.classList.toggle('on', show);
     show = !show;
-})
+});
 
 menuNav.addEventListener('click', ()=> {
     menuSection.classList.remove('on');
     document.body.style.overflow = 'initial';
     show = !show;
-})
+});
+
+
+// scroll
+const menuNavScroll = document.querySelectorAll('.menu-nav a[href^="#"]');
+
+menuNavScroll.forEach(item => {
+    item.addEventListener('click', scrollPara);
+});
+
+function scrollPara(evento) {
+    evento.preventDefault();
+    console.log(evento);
+    const para = scrollParaOtopoPeloId(evento.target)
+
+    scrollParaPosicao(para);
+};
+
+function scrollParaOtopoPeloId(elemento) {
+    const id = elemento.getAttribute('href');
+    return document.querySelector(id).offsetTop;
+};
+
+function scrollParaPosicao(para) {
+    window.scroll({
+        top: para,
+        behavior: 'smooth'
+    });
+};
